358e289782d9752fbb86ee16d1ebce80006718c4
Rahul R <127472805+Rahul-R-1796@users.noreply.github.com>
2024-02-01 00:15:46 +0530

    Add files via upload

---
+++ src/actions.js
@@ -0,0 +1,15 @@
+export const addEmployee = (employee) => ({
+    type: 'ADD_EMPLOYEE',
+    payload: employee,
+  });
+  
+  export const updateEmployee = (employee) => ({
+    type: 'UPDATE_EMPLOYEE',
+    payload: employee,
+  });
+  
+  export const deleteEmployee = (id) => ({
+    type: 'DELETE_EMPLOYEE',
+    payload: id,
+  });
+  
