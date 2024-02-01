075270bd32e1fe9a0f96785dab5a679e221dc4b4
Rahul R <127472805+Rahul-R-1796@users.noreply.github.com>
2024-02-01 11:15:33 +0530

    Update CommonFooter.jsx

--- src/CommonFooter.jsx
+++ src/CommonFooter.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CommonFooter = () => {
   return (
     <footer className="mt-5 p-3 bg-light text-center relative-bottom">
-      <p>&copy; 2024 Company Name. All rights reserved.</p>
+      <p>&copy; 2024 - All rights reserved.</p>
     </footer>
   );
 };
